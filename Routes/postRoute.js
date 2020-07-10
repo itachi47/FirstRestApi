c0d78e38823a5a2b18c52b9f4e5719130cf86407
Kuldeep Singh Karhana <kuldeepkarhana21@gmail.com>
2020-07-10 18:07:49 +0530

    Fixed some bug

--- Routes/postRoute.js
+++ Routes/postRoute.js
@@ -5,10 +5,13 @@ const postModel = require('../Models/postModel');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('we are on post');
+});
 
 //POST A POST
 router.post('/', async (req, res) => {
-    const post = new postModelgit({
+    const post = new postModel ({
         title: req.body.title,
         description: req.body.description
 
